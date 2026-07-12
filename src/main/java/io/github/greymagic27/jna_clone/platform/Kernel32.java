8a3dab50cea9145c818f11dcb6c38a323009554d
Greymagic27 <tolly67p@gmail.com>
2026-07-12 19:06:05 +0100

    Start on javadoc process to see how it looks

--- src/main/java/io/github/greymagic27/jna_clone/platform/Kernel32.java
+++ src/main/java/io/github/greymagic27/jna_clone/platform/Kernel32.java
@@ -7,11 +7,29 @@ import io.github.greymagic27.jna_clone.WinNT.HANDLE;
 public interface Kernel32 extends Library {
     Kernel32 INSTANCE = Library.load(Kernel32.class);
 
+    /**
+     * Retrieves a pseudo handle for the current process.
+     * @return Pseudo handle to the current process.
+     */
     HANDLE GetCurrentProcess();
 
+    /**
+     * Retrieves a module handle for the specified module. The module must have been loaded by the calling process.
+     * @param lpModuleName Name of the loaded module (either a .dll for .exe file)
+     * @return Handle to the specified module. If the function fails, this will be NULL
+     */
     HMODULE GetModuleHandleW(String lpModuleName);
 
+    /**
+     * Retrieves the process identifier of the calling process.
+     * @return Process identifier of the calling process.
+     */
     int GetCurrentProcessId();
 
+    /**
+     * Retrieves the process identifier of the specified process.
+     * @param Process Handle to the process
+     * @return Process identifier. If the function fails, this will be zero
+     */
     int GetProcessId(HANDLE Process);
 }
